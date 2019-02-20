864478e88a948f0bae2dd2567600262ba6004b70
Max Bos <maxn.bos@gmail.com>
2019-02-20 10:54:17 +0100

    Update inspect_height_maps.m

--- Assignment 1/photometric/inspect_height_maps.m
+++ Assignment 1/photometric/inspect_height_maps.m
@@ -1,9 +1,10 @@
 function inspect_height_maps(albedo, normals, col_hm, row_hm, avg_hm)
     function inspect_height_map(hm, title_text)
         [hgt, wid] = size(hm);
+        H = rot90(fliplr(hm), 2);
         [X,Y] = meshgrid(1:12:wid, 1:12:hgt);
-        hm(hm < 5) = NaN;
-        Z = hm(1:12:end, 1:12:end);
+        H(H < 5) = NaN;
+        Z = H(1:12:end, 1:12:end);
 %         Z = Y.^2 - X.^2; 
 %         H = rot90(fliplr(hm), 2);
         [U,V,W] = surfnorm(X,Y,Z);
@@ -12,11 +13,15 @@ function inspect_height_maps(albedo, normals, col_hm, row_hm, avg_hm)
 %         quiver3(Z,U,V,W)
 %         surfnorm(X, Y, H)
         axis equal;
-        xlabel('Z')
+        xlabel('Y')
         ylabel('X')
-        zlabel('Y')
+        zlabel('Z')
         title(title_text)
         view(-50,40)
+        set(gca, 'XDir', 'reverse')
+%         set(gca, 'XTick', []);
+%         set(gca, 'YTick', []);
+%         set(gca, 'ZTick', []);
         saveas(gcf,strcat(title_text,'integrationshape.png'))
     end
 
