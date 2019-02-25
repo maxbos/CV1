fe59e6a3029684e69ad698cf41ab05658c1ffb85
Max Bos <maxn.bos@gmail.com>
2019-02-25 20:50:24 +0100

    Worked on 4.3.1.

--- Assignment 2/Part-1-Image Enhancement/main.m
+++ Assignment 2/Part-1-Image Enhancement/main.m
@@ -23,3 +23,26 @@ diff = result_2d_filter - result_1d_filter;
 plot(diff);
 title('difference between 1D and 2D filter conv. result');
 saveas(gcf, '3.1.2-difference.png');
+
+%% 4.3.1 First-order derivative filters
+% Using your implemented function compute gradient on image2.jpg, display
+% the following figures: 1. The gradient of the image in the x-direction. 
+% 2. The gradient of the image in the y-direction. 3. The gradient 
+% magnitude of each pixel. 4. The gradient direction of each pixel.
+clear all;
+og_im = imread('images/image2.jpg');
+
+[Gx, Gy, im_magnitude, im_direction] = compute_gradient(og_im);
+
+imwrite(og_im, '4.3.1-og_im.png');
+imwrite(Gx, '4.3.1-Gx.png');
+imwrite(Gy, '4.3.1-Gy.png');
+imwrite(im_magnitude, '4.3.1-im_magnitude.png');
+imwrite(im_direction, '4.3.1-im_direction.png');
+
+figure(1);
+subplot(2,3,1); imshow(og_im); title('original');
+subplot(2,3,2); imshow(Gx); title('applying 1D filter');
+subplot(2,3,3); imshow(Gy); title('applying 2D filter');
+subplot(2,3,4); imshow(im_magnitude); title('applying 2D filter');
+subplot(2,3,5); imshow(im_direction); title('applying 2D filter');
