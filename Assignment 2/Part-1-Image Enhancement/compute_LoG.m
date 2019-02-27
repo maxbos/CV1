424be604e7c87b3e965658f987e74b11b892c59f
ErikStammes <erik.stammes@student.uva.nl>
2019-02-27 11:19:04 +0100

    Tested 4.3.1 and completed 4.3.2

--- Assignment 2/Part-1-Image Enhancement/compute_LoG.m
+++ Assignment 2/Part-1-Image Enhancement/compute_LoG.m
@@ -3,16 +3,21 @@ function imOut = compute_LoG(image, LOG_type)
 switch LOG_type
     case 1
         %method 1
-        fprintf('Not implemented\n')
-
+        h1 = fspecial('gaussian', 5, 0.5);
+        h2 = fspecial('laplacian');
+        image = conv2(image, h1);
+        imOut = conv2(image, h2);
     case 2
         %method 2
-        fprintf('Not implemented\n')
-
+        h = fspecial('log', 5, 0.5);
+        imOut = conv2(image, h);
     case 3
         %method 3
-        fprintf('Not implemented\n')
-
+        h1 = fspecial('gaussian', 5, 0.5);
+        im1 = conv2(image, h1);
+        h2 = fspecial('gaussian', 5, 0.8);
+        im2 = conv2(image, h2);
+        imOut = im2 - im1;
 end
 end
 
