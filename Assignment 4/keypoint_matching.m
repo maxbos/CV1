694040fa84b17f1d06261c46048e7cc44667ca3c
ErikStammes <erik.stammes@student.uva.nl>
2019-03-07 12:20:39 +0100

    Update to keypoint matching

--- Assignment 4/keypoint_matching.m
+++ Assignment 4/keypoint_matching.m
@@ -1,11 +1,10 @@
-function [keypoints, matches,scores] = keypoint_matching(img1,img2)
+function [fa, fb, matches,scores] = keypoint_matching(img1,img2)
 %keypoint_matching
 % Finds matching keypoints between 2 images
 Ia = imread(img1);
 Ib = imread(img2);
 [fa, da] = vl_sift(single(Ia));
 [fb, db] = vl_sift(single(Ib));
-keypoints = fa(1:2, :);
-[matches, scores] = vl_ubcmatch(da, db, 0);
+[matches, scores] = vl_ubcmatch(da, db);
 end
 
