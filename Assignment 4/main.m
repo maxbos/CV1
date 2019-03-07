448aa5f9bfe7e9ee5e26403e412961a30f227b86
ErikStammes <erik.stammes@student.uva.nl>
2019-03-07 15:05:18 +0100

    Fixed call to imwarp

--- Assignment 4/main.m
+++ Assignment 4/main.m
@@ -36,7 +36,8 @@ subplot(1, 2, 2); imshow(transformedImg);
 
 % Also, transform using MATLAB built-in `imwarp`, and compare the results
 % with own nearest-neigbor interpolation.
-matlabTransformedImg = imwarp(originalImg, transformationParams);
+tform = affine2d(transformationParams);
+matlabTransformedImg = imwarp(originalImg, tform);
 figure;
 subplot(1, 2, 1); imshow(originalImg);
 subplot(1,2,2); imshow(matlabTransformedImg);
