e897fea3b0c36108f6bce57a2bdd3f4220cb1012
Max Bos <maxn.bos@gmail.com>
2019-03-18 19:59:27 +0100

    Splits train dataset for visual vocabulary

--- Final project/main.m
+++ Final project/main.m
@@ -1,22 +1,36 @@
 %% Training phase
 % Feature extraction and description
+clear
+close all
+
 mode = 'densesampling'; % this can take the values 'keypoints' and 
                         % 'densesampling'
-train = open('stl10_matlab/train.mat');                   
-features = extractFeatures(train, mode);
-sizeF=size(features);
-newS=sizeF(1)*sizeF(3);
-resh=reshape(features,[newS,sizeF(2)]);
+train = open('stl10_matlab/train.mat');
+% Get a part of the train images for training the vocabulary cluster
+% centroids.
+totalNumberImgsVocabulary = 50;
+[vocabularyX, vocabularyY, ...
+    restIndices] = trainSplitForVocabulary(train, totalNumberImgsVocabulary);
+% Extract their SIFT descriptors from the images for building the
+% visual vocabulary.
+features = extractFeatures(vocabularyX, mode);
+sizeF = size(features);
+newS = sizeF(1)*sizeF(3);
+resh = reshape(features, [newS, sizeF(2)]);
 size(resh)
-% Building visual vocabulary
-[idx,C] = kmeans(double(resh),20);
-% Encoding visual features
-%%
-encodedIm=encodeImage(features,C);
-encodedIm
-% Representing images by frequencies
 
-% Classification
+% Building visual vocabulary.
+% Perform KMeans to find clusters of feature descriptors, to get the
+% cluster centers as visual word descriptors.
+clusterNumber = 10;
+[idx, C] = kmeans(double(resh), clusterNumber);
+
+% Encoding visual features and representing images by frequencies.
+% encodedImgs = encodeFeatures(features, C);
+% size(encodedImgs)
+
+%% Train the SVM Classifiers
+SVMModels = trainSVMs(C, train, restIndices, mode);
 
 %% Testing phase
 % Evaluation
