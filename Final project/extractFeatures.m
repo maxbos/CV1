647417a35c78078c9676a61ee2e0f08aa95c1631
Max Bos <maxn.bos@gmail.com>
2019-03-18 19:59:00 +0100

    Made extractFeatures solely for extracting features

--- Final project/extractFeatures.m
+++ Final project/extractFeatures.m
@@ -1,38 +1,24 @@
-function [desc] = extractFeatures(dataset,mode)
-%extractFeatures
-% Extracts features from a given dataset
-%              'airplane', 'bird', 'ship', 'horse', 'car'];
-classlabels = [1,           2,      9,      7,       3];
-if size(dataset.X) == [5000,27648]
-    % train
-    X = reshape(dataset.X,5000,96,96,3);
-else
-    % test
-    X = reshape(dataset.X,8000,96,96,3);
-end
-
-class_indices = ismember(dataset.y, classlabels);
-X = X(class_indices,:,:,:);
-
-% Test with first 50 images
-nImages = 50;
-
-desc = [];
-for i=1:nImages;
-   image = squeeze(X(i,:,:,:));
-   if mode == 'densesampling'
-       [fa, da] = vl_dsift(single(rgb2gray(image)));
-       desc=[desc;da];
-      
-   else
-       % TODO
-        [fa, da] = vl_sift(single(rgb2gray(image)));
-        desc=[desc;da];
-   end
-   
-end
-sizeF=size(desc);
-desc=reshape(desc,[nImages,128,sizeF(2)]);
+function desc = extractFeatures(X, mode)
+% extractFeatures
+    nImages = size(X, 1)
+    % For a subset of size `nImages` of the qualified images,
+    % extract the features.
+    desc = [];
+    for i=1:nImages
+        image = squeeze(X(i,:,:,:));
+        if mode == 'densesampling'
+            % TODO: Perform smoothing?
+            [fa, da] = vl_dsift(single(rgb2gray(image)));
+            desc = [desc; da];
+        else
+            % TODO
+            [fa, da] = vl_sift(single(rgb2gray(image)));
+            desc = [desc; da];
+        end
+    end
+    
+    sizeF = size(desc);
+    desc = reshape(desc,[nImages,128,sizeF(2)]);
 
 end
 
