cd5d5a01ae9ce947222445bf565018db6caac9c9
ErikStammes <erik.stammes@student.uva.nl>
2019-03-22 11:14:51 +0100

    Minor cleanup for part-1

--- Final project/part-1/classIndices.m
+++ Final project/part-1/classIndices.m
@@ -1,6 +1,5 @@
 function indices = classIndices(dataset)
     indices = struct;
-    % Extracts features from a given dataset
     %              'airplane', 'bird', 'ship', 'horse', 'car'];
     classLabels = [1,           2,      9,      7,       3];
     [~, locationClasslabels] = ismember(dataset.y, classLabels);
--- Final project/part-1/main.m
+++ Final project/part-1/main.m
@@ -7,37 +7,27 @@
 clear
 close all
 
-profile clear
-profile on
-
 mode = 'gray'; % this can take the values 'gray', 'rgb' and 'opponent'
 train = open('stl10_matlab/train.mat');
-% Get a part of the train images for training the vocabulary cluster
-% centroids.
-totalNumberImgsVocabulary = 100*5;
-[vocabularyX, vocabularyY, ...
-    restIndices] = trainSplitForVocabulary(train, totalNumberImgsVocabulary);
-% Extract their SIFT descriptors from the images for building the
-% visual vocabulary.
+% Get a part of the train images for training the vocabulary cluster centroids.
+totalNumberImgsVocabulary = 500;
+[vocabularyX, vocabularyY, restIndices] = trainSplitForVocabulary(train, totalNumberImgsVocabulary);
+% Extract their SIFT descriptors from the images for building the visual vocabulary.
 features = extractFeatures(vocabularyX, mode);
 sizeF = size(features);
 newS = sizeF(1)*sizeF(3);
 resh = double(reshape(features, [newS, sizeF(2)]));
-profile report
 
 %% Calculate cluster centroids
 % Building visual vocabulary.
 % Perform KMeans to find clusters of feature descriptors, to get the
 % cluster centers as visual word descriptors.
-profile clear
-profile on
 clusterNumber = 400;
 [idx, C] = kmeans(resh, clusterNumber);
 
 %% Encoding visual features and representing images by frequencies.
 encodedImgs = encodeFeatures(features, C);
 size(encodedImgs)
-profile report
 
 %% Calculate cluster centroids using VL
 % profile clear
