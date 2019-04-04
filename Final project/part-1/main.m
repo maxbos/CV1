d86de19a6940129d14023e571d9abeb43120f80d
JanZ <janzuiderveld@gmail.com>
2019-04-04 02:47:38 +0200

    Update main.m

--- Final project/part-1/main.m
+++ Final project/part-1/main.m
@@ -73,3 +73,18 @@ demo(["opponent", "dense"], 1000, 1000, 800*5)
 demo(["gray", "dense"], 1000, 4000, 800*5)
 demo(["rgb", "dense"], 1000, 4000, 800*5)
 demo(["opponent", "dense"], 1000, 4000, 800*5)
+
+%% Cluster size: 400
+demo(["gray", "keypoints"], 1000, 400, 800*5)
+demo(["rgb", "keypoints"], 1000, 400, 800*5)
+demo(["opponent", "keypoints"], 1000, 400, 800*5)
+
+% Cluster size: 1000
+demo(["gray", "keypoints"], 1000, 1000, 800*5)
+demo(["rgb", "keypoints"], 1000, 1000, 800*5)
+demo(["opponent", "keypoints"], 1000, 1000, 800*5)
+
+% Cluster size: 4000
+demo(["gray", "keypoints"], 1000, 4000, 800*5)
+demo(["rgb", "keypoints"], 1000, 4000, 800*5)
+demo(["opponent", "keypoints"], 1000, 4000, 800*5)
